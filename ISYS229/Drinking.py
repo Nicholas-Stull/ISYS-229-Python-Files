e436912dd5bcd131d69c69911f4cd71ccf123296
Nicholas Stull <Nicholasp.stull@gmail.com>
2023-01-09 18:51:59 -0500

    Add project files.

---
+++ ISYS229/Drinking.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+
+userName = input("What is your name? ")
+userAge = input("How old are you? ")
+
+print("Hello " + userName)
+   
+if int(userAge) >= 21:
+    print("Lets go drink!")
+elif 18 <= int(userAge) <= 21: 
+    print("Can't Drink today, head Go to Canada")
+else:
+    print("Not Today")
+sleep(1)
+print("Don't Drink and Drive")
+#if 10000 <= number <= 30000:
+#elif int(userAge) 18 >= 21:
