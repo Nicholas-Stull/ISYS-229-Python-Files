32f7fb8199eaf0143a1c17ef0c5d5014e77e1282
Nicholas Stull <Nicholasp.stull@gmail.com>
2023-01-11 15:55:21 -0500

    Updated Drinking.py

--- 01092023/Drinking.py
+++ 01092023/Drinking.py
@@ -3,17 +3,15 @@ from time import sleep
 
 userName = input("What is your name? ")
 userAge = input("How old are you? ")
-
+dont = "Don't drink and drive"
+drive = "Drive sober"
 print("Hello " + userName)
    
 if int(userAge) >= 21:
     print("Lets go drink!")
 elif 18 <= int(userAge) <= 21: 
-    print("Can't Drink today, head Go to Canada")
+    print("Can't drink today, try in Canada")
 else:
-    print("Not Today")
-sleep(1)
-print("Don't Drink and Drive")
-print("Drive Sober")
-#if 10000 <= number <= 30000:
-#elif int(userAge) 18 >= 21:
+    print("you can't drink")
+#sleep(1)
+
