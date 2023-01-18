ec47d77d26d756c88f623cd222ca36ad04a0b41b
Nicholas Stull <Nicholasp.stull@gmail.com>
2023-01-18 17:20:13 -0500

    Class finished for 1/18/23

--- 01092023/Drinking.py
+++ 01092023/Drinking.py
@@ -9,11 +9,12 @@ print("Hello " + userName)
 if int(userAge) >= 21:
     print("Lets go drink!")
 elif 18 <= int(userAge) <= 21: 
-    print("Can't Drink today, head Go to Canada")
+    print("Can't Drink today, head to Canada")
 else:
     print("Not Today")
 sleep(1)
 print("Don't Drink and Drive")
+sleep(1)
 print("Drive Sober")
 #if 10000 <= number <= 30000:
 #elif int(userAge) 18 >= 21:
---
+++ 01182023/Week2_Template.py
@@ -0,0 +1,24 @@
+# import date class from datetime module
+
+
+
+# create the date object of today's date and legal age requirement variable
+
+
+
+
+#collecting user input regarding their first and last name along with their current age
+
+
+#create a function that calculates the difference between legal age and current age and then returns a value
+
+
+#print user information ensuring to capitalize user's first and last name using the f function. The user's age should also be printed on the same sentence
+
+
+
+#Develop an if statement to determine if the user is too young to run for president
+#If the user is old enough, you should print a statement that states "You are old enough to run for president"
+#If the user is not old enough, you should develop an else statement that calls a function to determine the age gap and then print the following
+#Wait until you turn (legal age variable) years old to run for the office of President. Which should be in about (function variable) years from (today's date variable)
+
